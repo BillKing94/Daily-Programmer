023e39ae59e8678b9a6323d1b95c2d93caea616b
Bill King <bill@billking.io>
2015-11-02 19:20:35 -0500

    Added A Game of Threes

---
+++ A Game of Threes/A Game of Threes/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://www.reddit.com/r/dailyprogrammer/comments/3r7wxz/20151102_challenge_239_easy_a_game_of_threes/
+
+using System;
+
+namespace A_Game_of_Threes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n;
+            if (args.Length < 1 || !uint.TryParse(args[0], out n))
+            {
+                Console.WriteLine($@"Usage: ""{AppDomain.CurrentDomain.FriendlyName}"" <uint>");
+                return;
+            }
+
+            int[] modifiers = { 0, -1, 1 };
+
+            while (n != 1)
+            {
+                Console.WriteLine($"{n} {modifiers[n % 3]}");
+                n = (n + 1) / 3; // NOTE: don't actually need to use the modifier... just truncate
+            }
+
+            Console.WriteLine("1");
+        }
+    }
+}
